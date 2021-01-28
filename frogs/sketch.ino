64a58f76f6a7710bf1f1be2d69040d1818c14ae1
Alfred Sterphone <alfred@sterphone.com>
2021-01-28 00:01:29 -0500

    some pseudocode

--- frogs/sketch.ino
+++ frogs/sketch.ino
@@ -3,5 +3,16 @@ void setup() {
 }
 
 void loop() {
-  
+  // if double-clicked toggle lily pad / frog
+  // -------
+  // if lily pad
+  // -- always send out lily pad message
+  // -- if triple-clicked, toggle golden
+  // -- if frog attached, randomize breakage
+  // -- if breakage, send broke message and turn to water
+  // if frog 
+  // -- always send out frog message
+  // -- if clicked, increase
+  // -- if lily pad message rx, minus one energy energy
+  // -- if broke rx, minus one energy
 }
