5cf49d31bcb524ba57a21c0edc8ef98da33a7b58
Alfred Sterphone <alfred@sterphone.com>
2021-01-28 19:23:23 -0500

    initial coding session with julia

--- frogs/sketch.ino
+++ frogs/sketch.ino
@@ -1,21 +1,62 @@
 
+enum BlinkStates {
+  LILY_PAD,
+  GOLDEN_LILY_PAD,
+  FROG,
+  WATER
+}
 
+enum Messages {
+  NO_MSG,
+  I_AM_A_LILY_PAD,
+  I_AM_A_FROG,
+  I_AM_A_GOLDEN_LILY_PAD,
+  I_AM_WATER
+}
+
+byte blinkState = BlinkStates::LILY_PAD;
 
 void setup() {
-
 }
 
 void loop() {
   // if double-clicked toggle lily pad / frog
+  if (buttonDoubleClicked()) {
+    if (blinkState == BlinkStates::LILY_PAD || blinkState == BlinkStates::GOLDEN_LILY_PAD) {
+      blinkState = BlinkStates::FROG;
+    } else if (blinkState == BlinkStates::FROG) {
+      blinkState = BlinkStates::LILY_PAD;
+    } else {
+      // future token states?
+    }
+  }
+
+  // if triple-clicked, toggle golden
+  if (buttonMultiClicked() && buttonClickCount == 3) {
+    if (blinkState == BlinkStates::LILY_PAD) {
+      blinkState = BlinkStates::GOLDEN_LILY_PAD;
+    } else if (blinkState == BlinkStates::GOLDEN_LILY_PAD) {
+      blinkState = BlinkStates::LILY_PAD;
+    } else {
+      // future states?
+    }
+  }
+
   // -------
+
   // if lily pad
-  // -- always send out lily pad message
-  // -- if triple-clicked, toggle golden
-  // -- if frog attached, randomize breakage
-  // -- if breakage, send broke message and turn to water
-  // if frog 
-  // -- always send out frog message
-  // -- if clicked, increase
-  // -- if lily pad message rx, minus one energy energy
-  // -- if broke rx, minus one energy
+  if (blinkState == BlinkStates::LILY_PAD) {
+    // always send out lily pad message
+    setValueSentOnAllFaces(Messages::I_AM_A_LILY_PAD);
+    // -- if frog attached, randomize breakage
+    // -- if breakage, send broke message and turn to water
+  }
+
+  if (blinkState == BlinkStates::FROG) {
+    // always send out frog message
+    setValueSentOnAllFaces(Messages::I_AM_A_FROG);
+    // -- if clicked, increase
+    // -- if lily pad message rx, minus one energy energy
+    // -- if broke rx, minus one energy
+  }
 }
