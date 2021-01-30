d2d31f9f0262cc73d6520abf32c9fd6b2b633033
Alfred Sterphone <alfred@sterphone.com>
2021-01-29 19:33:34 -0500

    eod commit

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -15,10 +15,12 @@ enum Messages {
 };
 
 byte blinkState = BlinkStates::LILY_PAD;
+bool didFrogLand = false;
+byte frogEnergy = 0;
 
 void setup() {
+  randomize();
 }
-
 
 void loop() {
   // if double-clicked toggle lily pad / frog
@@ -50,15 +52,37 @@ void loop() {
     // always send out lily pad message
     setValueSentOnAllFaces(Messages::I_AM_A_LILY_PAD);
     // -- if frog attached, randomize breakage
+    FOREACH_FACE(f) {
+      if (getLastValueReceivedOnFace(f) == Messages::I_AM_A_FROG) {)
+        if (didLilyPadBreak() && !didFrogLand) {
+          blinkState = BlinkStates::WATER;
+        }
+        else if (didFrogLand) {
+          // check to see if lily pad breaks from staying on it too long
+        }
+        didFrogLand = true;
+      }
+    }
     // -- if breakage, send broke message and turn to water
+  }
+
+  if (blinkState == BlinkStates::GOLDEN_LILY_PAD) {
+    setValueSentOnAllFaces(Messages::I_AM_A_LILY_PAD);
   }
 
   if (blinkState == BlinkStates::FROG) {
     // always send out frog message
     setValueSentOnAllFaces(Messages::I_AM_A_FROG);
-    // -- if clicked, increase
+    // -- if clicked
+    if (buttonSingleClicked()) {
+      determineFrogEnergy();
+    }
     // -- if lily pad message rx, minus one energy energy
     // -- if broke rx, minus one energy
+  }
+
+  if (blinkState == BlinkStates::WATER) {
+    setValueSentOnAllFaces(Messages::I_AM_WATER);
   }
 
   displayLoop();
@@ -74,6 +98,7 @@ void displayLoop() {
       setColor(GREEN);
       setColorOnFace(OFF, 0);
       setColorOnFace(OFF, 3);
+      showEnergy();
       break;
     case BlinkStates::GOLDEN_LILY_PAD:
       setColor(YELLOW);
@@ -83,4 +108,23 @@ void displayLoop() {
       setColor(BLUE);
       break;
   }
+}
+
+bool didLilyPadBreak() {
+  short randomNumber = random(100);
+  if (random % 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void determineFrogEnergy() {
+  while (frogEnergy == 0) {
+    frogEnergy = (1 + random(99)) % 4;
+  }
+}
+
+void showEnergy() {
+  // millis()
 }
